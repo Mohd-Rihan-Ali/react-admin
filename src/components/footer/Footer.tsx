c2e1155725de2fa988b29c6e1619f566ed031917
Rihan <mohdrihanali4130@gmail.com>
2023-08-04 23:33:16 +0530

    admin ui deceloped

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>theadmin</span>
+      <span>&#169; admin dashboard powered by TheAdmin.</span>
+    </div>
+  )
+}
+
+export default Footer
